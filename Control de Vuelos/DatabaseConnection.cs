e463fb7dbfdab787ef5e7bf41c76ae7a9c445064
Eyden S <esu@estudiantec.cr>
2024-05-21 23:52:45 -0600

    Creación de proyecto de visual studio

---
+++ Control de Vuelos/DatabaseConnection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Control_de_Vuelos {
+	internal class DatabaseConnection {
+		static readonly string cadena = "Data source=" + Environment.MachineName + "; Initial Catalog=AEROLINEAS; Integrated Security=True";
+
+		public SqlConnection ConectarBD = new SqlConnection();
+
+		public DatabaseConnection() {
+			ConectarBD.ConnectionString = cadena;
+		}
+
+		public bool open() {
+			try {
+				ConectarBD.Open();
+				return true;
+			} catch (Exception) {
+				return false;
+			}
+		}
+
+		public void close() {
+			ConectarBD.Close();
+		}
+	}
+}
+
