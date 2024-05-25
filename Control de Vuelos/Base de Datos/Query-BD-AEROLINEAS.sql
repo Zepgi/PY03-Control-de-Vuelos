9ad2301453d40d98c95832dfd40328a6c87b9106
Eyden S <esu@estudiantec.cr>
2024-05-24 22:34:21 -0600

    Creación de tablas de la base de datos

--- Control de Vuelos/Base de Datos/Query-BD-AEROLINEAS.sql
+++ Control de Vuelos/Base de Datos/Query-BD-AEROLINEAS.sql
@@ -3,3 +3,37 @@ GO
 
 USE AEROLINEAS;
 GO
+
+CREATE TABLE Aerolineas
+	(idAerolinea	INT	IDENTITY(1,1)	NOT NULL,
+	nombre			VARCHAR(100)		NOT NULL,
+	lema			VARCHAR(MAX)		NOT NULL,
+	PRIMARY KEY (idAerolinea));
+GO
+
+CREATE TABLE Aviones
+	(idAvion			INT	IDENTITY(1,1)	NOT NULL,
+	marca				VARCHAR(100)		NOT NULL,
+	matricula			VARCHAR(50)	UNIQUE	NOT NULL,
+	capacidadPasajeros	INT					NOT NULL,
+	CHECK (capacidadPasajeros>=35),
+	PRIMARY KEY (idAvion));
+GO
+
+CREATE TABLE AvionesAerolinea
+	(id				INT	IDENTITY(1,1)	NOT NULL,
+	matricula		VARCHAR(50)			NOT NULL,
+	idAerolinea		INT,
+	PRIMARY KEY(id),
+	FOREIGN KEY (matricula)		REFERENCES Aviones(matricula),
+	FOREIGN KEY (idAerolinea)	REFERENCES Aerolineas(idAerolinea));
+GO
+
+CREATE TABLE Pilotos
+	(idPiloto		INT	IDENTITY(1,1)	NOT NULL,
+	cedulaPiloto	VARCHAR(150)	NOT NULL	UNIQUE,
+	apellidoPat		VARCHAR(150)	NOT NULL,
+	apellidoMat		VARCHAR(150)	NOT NULL,
+	nombre			VARCHAR(150)	NOT NULL,
+	PRIMARY KEY(idPiloto));
+GO
