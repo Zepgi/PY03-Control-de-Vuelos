a5a0bad0b9922a63a71ef8d3b68f4426ca52e4b3
Eyden S <esu@estudiantec.cr>
2024-05-24 23:28:40 -0600

    insercion de llaves foraneas

--- Control de Vuelos/Base de Datos/Query-BD-AEROLINEAS.sql
+++ Control de Vuelos/Base de Datos/Query-BD-AEROLINEAS.sql
@@ -38,35 +38,47 @@ CREATE TABLE Pilotos
 	PRIMARY KEY(idPiloto));
 GO
 
+CREATE TABLE Ciudades();
+
+GO
+
+
+CREATE TABLE Vuelos(
+	PRIMARY KEY(idVuelo),
+	idVuelo				INT IDENTITY(1,1)	NOT NULL,
+	idAvion				INT					NOT NULL,
+	cedulaPiloto		VARCHAR(100) UNIQUE	NOT NULL,
+	fechaHoraPartida	DATETIME			NOT NULL,
+	fechaHoraLlegada	DATETIME			NOT NULL,
+	codigoCiudadPartida	VARCHAR(100)		NOT NULL,
+	codigoCiudadDestino	VARCHAR(100)		NOT NULL,
+	FOREIGN KEY	(cedulaPiloto)	REFERENCES Pilotos(cedulaPiloto),
+	FOREIGN KEY	(idAvion)		REFERENCES Aviones(idAvion),
+	FOREIGN KEY	(ciudadPartida)	REFERENCES Ciudades(codigoCiudad),
+	FOREIGN KEY	(ciudadDestino)	REFERENCES Ciudades(codigoCiudad));
+
+GO
 
 CREATE TABLE Pasajeros(
- PRIMARY KEY(idPasajero),
- idPasajero INT IDENTITY(1,1) NOT NULL,
- cedulaPasajero VARCHAR(20) UNIQUE NOT NULL,
- nombre VARCHAR(20) NOT NULL,
- apellidoMat VARCHAR(20) NOT NULL,
- apellidoPat VARCHAR(20) NOT NULL,
- ciudad VARCHAR(100) NOT NULL);
+	PRIMARY KEY(idPasajero),
+	idPasajero		INT IDENTITY(1,1)	NOT NULL,
+	cedulaPasajero	VARCHAR(100) UNIQUE	NOT NULL,
+	nombre			VARCHAR(20)			NOT NULL,
+	apellidoMat		VARCHAR(20)			NOT NULL,
+	apellidoPat		VARCHAR(20)			NOT NULL,
+	codigoCiudad	VARCHAR(100)		NOT NULL,
+	FOREIGN KEY	(ciudad) REFERENCES Ciudades(codigoCiudad));
 
 GO
 
 CREATE TABLE ListaPasajeros(
-primary key(id),
-id INT IDENTITY(1,1) NOT NULL,
-idVuelo INT NOT NULL,
-cedulaPasajero VARCHAR(20) UNIQUE NOT NULL,
-confirmado BIT NOT NULL);
+	PRIMARY KEY(id),
+	id				INT IDENTITY(1,1)	NOT NULL,
+	idVuelo			INT					NOT NULL,
+	cedulaPasajero	VARCHAR(100) UNIQUE	NOT NULL,
+	confirmado		BIT					NOT NULL,
+	FOREIGN KEY	(idVuelo)		 REFERENCES Vuelos(idVuelo),
+	FOREIGN KEY (cedulaPasajero) REFERENCES Pasajeros(cedulaPasajero));
 
 GO
 
-CREATE TABLE Vuelos(
-PRIMARY KEY(idVuelo),
-idVuelo INT IDENTITY(1,1) NOT NULL,
-idAvion INT NOT NULL,
-cedulaPiloto VARCHAR(30) UNIQUE NOT NULL,
-fechaHoraPartida DATETIME NOT NULL,
-fechaHoraLlegada DATETIME NOT NULL,
-ciudadPartida VARCHAR(100) NOT NULL,
-ciudadDestino VARCHAR(100) NOT NULL);
-
-GO
