1c81e29dfef6e9525959b9ff9772befbc88aae0f
Eyden S <esu@estudiantec.cr>
2024-05-26 16:12:20 -0600

    correción datos, nombres atributos , inserciones hacia la bd

--- Control de Vuelos/Base de Datos/Query-BD-AEROLINEAS.sql
+++ Control de Vuelos/Base de Datos/Query-BD-AEROLINEAS.sql
@@ -41,7 +41,7 @@ GO
 
 CREATE TABLE Roles
 	(idRol INT IDENTITY(1,1) NOT NULL,
-	codigoRol INT UNIQUE NOT NULL,
+	codigoRol VARCHAR(100) UNIQUE NOT NULL,
 	nombreRol VARCHAR(100)NOT NULL,
 	PRIMARY KEY(idRol));
 GO
@@ -49,20 +49,20 @@ GO
 CREATE TABLE Usuarios 
 	(idUsuario INT IDENTITY(1,1) NOT NULL,
 	cedulaUsuario VARCHAR(150) UNIQUE NOT NULL,
-	codigoRol INT NOT NULL,
+	codigoRol VARCHAR(100) NOT NULL,
 	nombre VARCHAR(150) NOT NULL,
 	apellidoPat VARCHAR(150) NOT NULL,
 	apellidoMat VARCHAR(150) NOT NULL,
 	fechaNacim DATE NOT NULL,
-	numTel VARCHAR(30) NOT NULL,
-	genero VARCHAR(10) NOT NULL,
+	numTel VARCHAR(50) NOT NULL,
+	genero VARCHAR(4) NOT NULL,
 	estado BIT NOT NULL,
 	PRIMARY KEY(idUsuario),
 	FOREIGN KEY(codigoRol) REFERENCES Roles(codigoRol));
 GO
 
 CREATE TABLE CuentaUsuario
-	(iCuenta INT IDENTITY(1,1) NOT NULL,
+	(idCuenta INT IDENTITY(1,1) NOT NULL,
 	idUsuario INT NOT NULL,
 	email VARCHAR(300) NOT NULL,
 	contrasenia VARCHAR(100) NOT NULL,
@@ -70,40 +70,40 @@ CREATE TABLE CuentaUsuario
 	FOREIGN KEY(idUsuario) REFERENCES Usuarios(idUsuario));
 GO
 
-CREATE TABLE Cuidades
-	(idCuidad INT IDENTITY(1,1) NOT NULL,
-	codigoCuidad VARCHAR(150) UNIQUE NOT NULL,
-	pais VARCHAR(300) NOT NULL,
-	canton VARCHAR(300) NOT NULL,
-	distrito VARCHAR(300) NOT NULL,
-	PRIMARY KEY(idCuidad));
+CREATE TABLE Ciudades
+	(idCiudad INT IDENTITY(1,1) NOT NULL,
+	codigoCiudad VARCHAR(150) UNIQUE NOT NULL,
+	pais VARCHAR(150) NOT NULL,
+	canton VARCHAR(150) NOT NULL,
+	distrito VARCHAR(150) NOT NULL,
+	PRIMARY KEY(idCiudad));
 GO
 
 CREATE TABLE Vuelos(
 	PRIMARY KEY(idVuelo),
 	idVuelo				INT IDENTITY(1,1)	NOT NULL,
 	idAvion				INT					NOT NULL,
-	cedulaPiloto		VARCHAR(100) UNIQUE	NOT NULL,
+	cedulaPiloto		VARCHAR(150) UNIQUE	NOT NULL,
 	fechaHoraPartida	DATETIME			NOT NULL,
 	fechaHoraLlegada	DATETIME			NOT NULL,
-	codigoCiudadPartida	VARCHAR(100)		NOT NULL,
-	codigoCiudadDestino	VARCHAR(100)		NOT NULL,
+	codigoCiudadPartida	VARCHAR(150)		NOT NULL,
+	codigoCiudadDestino	VARCHAR(150)		NOT NULL,
 	FOREIGN KEY	(cedulaPiloto)	REFERENCES Pilotos(cedulaPiloto),
 	FOREIGN KEY	(idAvion)		REFERENCES Aviones(idAvion),
-	FOREIGN KEY	(ciudadPartida)	REFERENCES Ciudades(codigoCiudad),
-	FOREIGN KEY	(ciudadDestino)	REFERENCES Ciudades(codigoCiudad));
+	FOREIGN KEY	(codigoCiudadPartida)	REFERENCES Ciudades(codigoCiudad),
+	FOREIGN KEY	(codigoCiudadDestino)	REFERENCES Ciudades(codigoCiudad));
 
 GO
 
 CREATE TABLE Pasajeros(
 	PRIMARY KEY(idPasajero),
 	idPasajero		INT IDENTITY(1,1)	NOT NULL,
-	cedulaPasajero	VARCHAR(100) UNIQUE	NOT NULL,
-	nombre			VARCHAR(20)			NOT NULL,
-	apellidoMat		VARCHAR(20)			NOT NULL,
-	apellidoPat		VARCHAR(20)			NOT NULL,
-	codigoCiudad	VARCHAR(100)		NOT NULL,
-	FOREIGN KEY	(ciudad) REFERENCES Ciudades(codigoCiudad));
+	cedulaPasajero	VARCHAR(150) UNIQUE	NOT NULL,
+	nombre			VARCHAR(150)			NOT NULL,
+	apellidoMat		VARCHAR(150)			NOT NULL,
+	apellidoPat		VARCHAR(150)			NOT NULL,
+	codigoCiudad	VARCHAR(150)		NOT NULL,
+	FOREIGN KEY	(codigoCiudad) REFERENCES Ciudades(codigoCiudad));
 
 GO
 
@@ -111,7 +111,7 @@ CREATE TABLE ListaPasajeros(
 	PRIMARY KEY(id),
 	id				INT IDENTITY(1,1)	NOT NULL,
 	idVuelo			INT					NOT NULL,
-	cedulaPasajero	VARCHAR(100) UNIQUE	NOT NULL,
+	cedulaPasajero	VARCHAR(150) UNIQUE	NOT NULL,
 	confirmado		BIT					NOT NULL,
 	FOREIGN KEY	(idVuelo)		 REFERENCES Vuelos(idVuelo),
 	FOREIGN KEY (cedulaPasajero) REFERENCES Pasajeros(cedulaPasajero));
@@ -132,6 +132,6 @@ GO
 INSERT INTO Usuarios
 	(cedulaUsuario, codigoRol, nombre, apellidoPat, apellidoMat, fechaNacim, numTel, genero, estado)
 VALUES
-	(1, 'ADM101', 'Eyden', 'Su', 'Díaz', '2004-17-09', 1, 'Masculino', 1),
-	(2, 'RSV302', 'Juana', 'Montes', 'Sanchez', '1995-12-2', 1, 'Femenino', 1);
+	(1, 'ADM101', 'Eyden', 'Su', 'Díaz', '2004-09-17', '1', 'M', 1),
+	(2, 'RSV302', 'Juana', 'Montes', 'Sanchez', '1995-12-2', '1', 'F', 1);
 GO
