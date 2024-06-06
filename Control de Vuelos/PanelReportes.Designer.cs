2b439287d16ab4a69a57828fab80c53c76f13381
Jose Andres Solano Vargas <josesolanovarga13@gmail.com>
2024-06-06 01:58:48 -0600

    Reportes ordenado

--- Control de Vuelos/PanelReportes.Designer.cs
+++ Control de Vuelos/PanelReportes.Designer.cs
@@ -23,7 +23,6 @@
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
-            this.label1 = new System.Windows.Forms.Label();
             this.dataGridViewAirPlanes_AirLines = new System.Windows.Forms.DataGridView();
             this.label2 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
@@ -36,27 +35,20 @@
             this.guna2TextBoxAir8 = new Guna.UI2.WinForms.Guna2TextBox();
             this.guna2ButtonAir8 = new Guna.UI2.WinForms.Guna2Button();
             this.label3 = new System.Windows.Forms.Label();
+            this.dataGridViewAir1 = new System.Windows.Forms.DataGridView();
+            this.dtDepartureDate1 = new Guna.UI2.WinForms.Guna2DateTimePicker();
+            this.buttonCheckFlys1 = new Guna.UI2.WinForms.Guna2Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAirPlanes_AirLines)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAire8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAir1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(10, 7);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(349, 26);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Haz ingresado al panel de reportes\r\n";
             // 
             // dataGridViewAirPlanes_AirLines
             // 
             this.dataGridViewAirPlanes_AirLines.BackgroundColor = System.Drawing.Color.White;
             this.dataGridViewAirPlanes_AirLines.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewAirPlanes_AirLines.Location = new System.Drawing.Point(32, 166);
+            this.dataGridViewAirPlanes_AirLines.Location = new System.Drawing.Point(12, 791);
             this.dataGridViewAirPlanes_AirLines.Name = "dataGridViewAirPlanes_AirLines";
             this.dataGridViewAirPlanes_AirLines.Size = new System.Drawing.Size(370, 214);
             this.dataGridViewAirPlanes_AirLines.TabIndex = 10;
@@ -66,7 +58,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(27, 125);
+            this.label2.Location = new System.Drawing.Point(7, 750);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(233, 26);
@@ -77,7 +69,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(27, 550);
+            this.label4.Location = new System.Drawing.Point(7, 1208);
             this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(337, 26);
@@ -88,7 +80,7 @@
             // 
             this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(560, 834);
+            this.dataGridView1.Location = new System.Drawing.Point(577, 1455);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(370, 170);
             this.dataGridView1.TabIndex = 14;
@@ -97,18 +89,18 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(644, 487);
+            this.label5.Location = new System.Drawing.Point(11, 88);
             this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(233, 26);
+            this.label5.Size = new System.Drawing.Size(249, 26);
             this.label5.TabIndex = 16;
-            this.label5.Text = "Aviones por aerolineas";
+            this.label5.Text = "Vuelos activos por fecha";
             // 
             // dataGridViewAire8
             // 
             this.dataGridViewAire8.BackgroundColor = System.Drawing.Color.White;
             this.dataGridViewAire8.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewAire8.Location = new System.Drawing.Point(32, 600);
+            this.dataGridViewAire8.Location = new System.Drawing.Point(12, 1258);
             this.dataGridViewAire8.Name = "dataGridViewAire8";
             this.dataGridViewAire8.Size = new System.Drawing.Size(370, 225);
             this.dataGridViewAire8.TabIndex = 17;
@@ -128,7 +120,7 @@
             this.searchAirLine3.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
             this.searchAirLine3.Font = new System.Drawing.Font("Century Gothic", 10.2F);
             this.searchAirLine3.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.searchAirLine3.Location = new System.Drawing.Point(74, 429);
+            this.searchAirLine3.Location = new System.Drawing.Point(54, 1054);
             this.searchAirLine3.Margin = new System.Windows.Forms.Padding(4);
             this.searchAirLine3.Name = "searchAirLine3";
             this.searchAirLine3.PasswordChar = '\0';
@@ -136,7 +128,6 @@
             this.searchAirLine3.SelectedText = "";
             this.searchAirLine3.Size = new System.Drawing.Size(145, 36);
             this.searchAirLine3.TabIndex = 27;
-
             // 
             // airline3
             // 
@@ -152,7 +143,7 @@
             this.airline3.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(216)))), ((int)(((byte)(143)))));
             this.airline3.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold);
             this.airline3.ForeColor = System.Drawing.Color.White;
-            this.airline3.Location = new System.Drawing.Point(232, 429);
+            this.airline3.Location = new System.Drawing.Point(212, 1054);
             this.airline3.Margin = new System.Windows.Forms.Padding(2);
             this.airline3.Name = "airline3";
             this.airline3.PressedColor = System.Drawing.Color.Teal;
@@ -165,7 +156,7 @@
             // 
             this.airlineLabel.AutoSize = true;
             this.airlineLabel.Font = new System.Drawing.Font("Century Gothic", 10.2F);
-            this.airlineLabel.Location = new System.Drawing.Point(71, 397);
+            this.airlineLabel.Location = new System.Drawing.Point(51, 1022);
             this.airlineLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.airlineLabel.Name = "airlineLabel";
             this.airlineLabel.Size = new System.Drawing.Size(157, 19);
@@ -186,7 +177,7 @@
             this.guna2TextBoxAir8.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
             this.guna2TextBoxAir8.Font = new System.Drawing.Font("Century Gothic", 10.2F);
             this.guna2TextBoxAir8.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.guna2TextBoxAir8.Location = new System.Drawing.Point(32, 882);
+            this.guna2TextBoxAir8.Location = new System.Drawing.Point(12, 1540);
             this.guna2TextBoxAir8.Margin = new System.Windows.Forms.Padding(4);
             this.guna2TextBoxAir8.Name = "guna2TextBoxAir8";
             this.guna2TextBoxAir8.PasswordChar = '\0';
@@ -209,7 +200,7 @@
             this.guna2ButtonAir8.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(216)))), ((int)(((byte)(143)))));
             this.guna2ButtonAir8.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold);
             this.guna2ButtonAir8.ForeColor = System.Drawing.Color.White;
-            this.guna2ButtonAir8.Location = new System.Drawing.Point(190, 882);
+            this.guna2ButtonAir8.Location = new System.Drawing.Point(170, 1540);
             this.guna2ButtonAir8.Margin = new System.Windows.Forms.Padding(2);
             this.guna2ButtonAir8.Name = "guna2ButtonAir8";
             this.guna2ButtonAir8.PressedColor = System.Drawing.Color.Teal;
@@ -222,19 +213,76 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Century Gothic", 10.2F);
-            this.label3.Location = new System.Drawing.Point(29, 850);
+            this.label3.Location = new System.Drawing.Point(9, 1508);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(157, 19);
             this.label3.TabIndex = 28;
             this.label3.Text = "Nombre de aerolinea";
             // 
+            // dataGridViewAir1
+            // 
+            this.dataGridViewAir1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridViewAir1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewAir1.Location = new System.Drawing.Point(12, 126);
+            this.dataGridViewAir1.Name = "dataGridViewAir1";
+            this.dataGridViewAir1.Size = new System.Drawing.Size(370, 214);
+            this.dataGridViewAir1.TabIndex = 31;
+            // 
+            // dtDepartureDate1
+            // 
+            this.dtDepartureDate1.Animated = true;
+            this.dtDepartureDate1.AutoRoundedCorners = true;
+            this.dtDepartureDate1.BackColor = System.Drawing.Color.White;
+            this.dtDepartureDate1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(216)))), ((int)(((byte)(143)))));
+            this.dtDepartureDate1.BorderRadius = 15;
+            this.dtDepartureDate1.Checked = true;
+            this.dtDepartureDate1.FillColor = System.Drawing.Color.White;
+            this.dtDepartureDate1.FocusedColor = System.Drawing.Color.Gainsboro;
+            this.dtDepartureDate1.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtDepartureDate1.Format = System.Windows.Forms.DateTimePickerFormat.Long;
+            this.dtDepartureDate1.Location = new System.Drawing.Point(24, 358);
+            this.dtDepartureDate1.Margin = new System.Windows.Forms.Padding(2);
+            this.dtDepartureDate1.MaxDate = new System.DateTime(9998, 12, 31, 0, 0, 0, 0);
+            this.dtDepartureDate1.MinDate = new System.DateTime(1753, 1, 1, 0, 0, 0, 0);
+            this.dtDepartureDate1.Name = "dtDepartureDate1";
+            this.dtDepartureDate1.Size = new System.Drawing.Size(232, 32);
+            this.dtDepartureDate1.TabIndex = 32;
+            this.dtDepartureDate1.Value = new System.DateTime(2024, 6, 3, 15, 3, 26, 864);
+            // 
+            // buttonCheckFlys1
+            // 
+            this.buttonCheckFlys1.Animated = true;
+            this.buttonCheckFlys1.AutoRoundedCorners = true;
+            this.buttonCheckFlys1.BackColor = System.Drawing.Color.Transparent;
+            this.buttonCheckFlys1.BorderColor = System.Drawing.Color.White;
+            this.buttonCheckFlys1.BorderRadius = 15;
+            this.buttonCheckFlys1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.buttonCheckFlys1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.buttonCheckFlys1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.buttonCheckFlys1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.buttonCheckFlys1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(216)))), ((int)(((byte)(143)))));
+            this.buttonCheckFlys1.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold);
+            this.buttonCheckFlys1.ForeColor = System.Drawing.Color.White;
+            this.buttonCheckFlys1.Location = new System.Drawing.Point(278, 358);
+            this.buttonCheckFlys1.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonCheckFlys1.Name = "buttonCheckFlys1";
+            this.buttonCheckFlys1.PressedColor = System.Drawing.Color.Teal;
+            this.buttonCheckFlys1.Size = new System.Drawing.Size(42, 32);
+            this.buttonCheckFlys1.TabIndex = 40;
+            this.buttonCheckFlys1.Text = "✔️";
+            this.buttonCheckFlys1.Visible = false;
+            this.buttonCheckFlys1.Click += new System.EventHandler(this.buttonCheckFlys1_Click);
+            // 
             // PanelReportes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.AutoScroll = true;
-            this.ClientSize = new System.Drawing.Size(993, 582);
+            this.ClientSize = new System.Drawing.Size(976, 582);
+            this.Controls.Add(this.buttonCheckFlys1);
+            this.Controls.Add(this.dtDepartureDate1);
+            this.Controls.Add(this.dataGridViewAir1);
             this.Controls.Add(this.guna2TextBoxAir8);
             this.Controls.Add(this.guna2ButtonAir8);
             this.Controls.Add(this.label3);
@@ -247,7 +295,6 @@
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.dataGridViewAirPlanes_AirLines);
-            this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximumSize = new System.Drawing.Size(976, 582);
@@ -258,14 +305,13 @@
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAirPlanes_AirLines)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAire8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAir1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
 		}
 
 		#endregion
-
-		private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridView dataGridViewAirPlanes_AirLines;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label4;
@@ -278,5 +324,8 @@
         private Guna.UI2.WinForms.Guna2TextBox guna2TextBoxAir8;
         private Guna.UI2.WinForms.Guna2Button guna2ButtonAir8;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridViewAir1;
+        public Guna.UI2.WinForms.Guna2DateTimePicker dtDepartureDate1;
+        private Guna.UI2.WinForms.Guna2Button buttonCheckFlys1;
     }
 }
